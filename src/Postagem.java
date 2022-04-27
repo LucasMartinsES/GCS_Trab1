b13f57825d53cb7219414f738e2158502809751b
Eduardo Bassani <99217468+eduardobassanii@users.noreply.github.com>
2022-04-27 15:16:11 -0300

    Adição de atributos da classe Postagem

--- src/Postagem.java
+++ src/Postagem.java
@@ -1,3 +1,10 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Postagem {
-    
+    private Usuarios usuario;
+    private LocalDateTime dataHora;
+    private String texto;
+    private ArrayList<String> tags;
+    private String link;
 }
