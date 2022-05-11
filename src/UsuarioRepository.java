cba0e1fe74b55727eff72ac08d0814d8d55d640a
joao@ubots.com.br <joao@ubots.com.br>
2022-05-11 20:35:30 -0300

    userRepo

---
+++ src/UsuarioRepository.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UsuarioRepository {
+    private final List<Usuario> usuarios;
+
+    public UsuarioRepository() {
+        usuarios = new ArrayList<>();
+    }
+
+    public Usuario getUser(String username) {
+        return this.usuarios.stream()
+                .filter(it -> it.getNome().equals(username))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Usuario connectUser(String username, Usuario.TipoUsuario tipoUsuario) {
+        Optional<Usuario> possivelUsuario = usuarios.stream()
+                .filter(it -> it.getNome().equals(username))
+                .findFirst();
+
+        if (possivelUsuario.isPresent()) return possivelUsuario.get();
+        Usuario usuario = new Usuario(username, tipoUsuario);
+        usuarios.add(usuario);
+        return usuario;
+    }
+}
