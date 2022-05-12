3227a56aa89b01cae6c28f0d1685b373dda527d4
jphrmartins <jphmartins@gmail.com>
2022-05-12 20:17:00 -0300

    só ajuste de user (#15)
    Co-authored-by: joao@ubots.com.br <joao@ubots.com.br>

--- src/UsuarioFuncionario.java
+++
@@ -1,7 +0,0 @@
-public class UsuarioFuncionario extends Usuarios {
-    
-    private String Funcionario;
-
-    public UsuarioFuncionario(String aIdentificador, String aNome) {
-        super(aIdentificador, aNome);
-}
