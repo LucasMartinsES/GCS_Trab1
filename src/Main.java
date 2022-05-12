53246d24000f4974d718cb815cbe27e321fc161d
joao@ubots.com.br <joao@ubots.com.br>
2022-05-12 20:12:57 -0300

    só ajuste de user

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        Usuarios u1 = new Usuarios("adm-111", "");
-        Usuarios u2 = new Usuarios("adm-222", "");
-
+        Usuario u1 = new UsuarioAdm("adm-111");
+        Usuario u2 = new UsuarioAdm("adm-222");
     }
 }
